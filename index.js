411942f31b15d6ac69b05fcb278c2ad75e781a1a
Andrew Storrow <andrew.storrow@gmail.com>
2023-02-18 16:59:29 -0800

    implemented dynamic rendering from javascript data

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: "21,492"
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: "12,502"
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: "15,137"
-    }
-]
+import { posts } from './data.js'
 
+//Constructs and returns the html of the post feed as a string
+function getFeedHtml() {
+    let feedHtml = ``;
+
+    //Construct post feed html
+    for (const post of posts) {
+
+        //Construct comments feed html
+        let commentsHtml = ``;
+        for (const comment of post.comments) {
+            commentsHtml += `
+<div class="comment">
+    <h2>${comment.username}</h2>
+    ${comment.comment}
+</div>
+            `;
+        }
+
+        feedHtml += `
+<section>
+    <div class="post-header">
+        <img src="${post.avatar}" alt="post author avatar" class="author-avatar">
+        <div class="post-author">
+            <h2>${post.name}</h2>
+            ${post.location}
+        </div>
+    </div>
+    <img src="${post.post}" alt="post image" class="post-img">
+    <div class="icons">
+        <a href="#"><img src="./images/icon-heart.png" alt="heart icon"></a>
+        <a href="#"><img src="./images/icon-comment.png" alt="comment icon"></a>
+        <a href="#"><img src="./images/icon-dm.png" alt="direct message icon"></a>
+    </div>
+    <h2 class="likes">${post.likes} likes</h2>
+    <div class="comments">
+        ${commentsHtml}
+    </div>
+</section>
+        `;
+    }
+
+    return feedHtml;
+}
+
+// Render the post feed
+function render() {
+    document.getElementById('feed').innerHTML = getFeedHtml();
+}
+
+render();
